865cd35aa7ae318a20ba55a62ffc4abc24ddb75f
조혜린 <hyer1205@gmail.com>
2024-09-04 23:20:28 +0900

    ✨feat: 다단계 칫솔 판매 문제 풀이
    review
    - 2일 붙잡은 문제였는데 사실 피곤하지 않은 상태에서 풀었다면 2일까지는 소요되지 않았을 것 같다.
    - 그리고 이번 문제를 풀 때, VSCode Run and Debug 기능을 애용했다. 디버깅을 하니까 어디서 막힌 건지를 파악하기 쉬웠다.
    - for 문을 사용해서 enroll 배열과 referral 배열을 순회하면서 sellerMap 에 값을 설정한다. {판매원: {이익: [], 부모 노드: new Node()}
    - Array.prototype.forEach() 메서드를 사용해서 seller 배열을 순회하며 각 노드가 얻는 이익을 earn 키의 값인 배열에 차곡차곡 삽입한다.
    - for of 문을 사용하여 sellerMap을 순회하며 answer 배열에 각 판매원들이 얻은 이익을 계산한 값을 넣고 반환한다.

--- solutions/29-다단계-칫솔-판매.js
+++ solutions/29-다단계-칫솔-판매.js
@@ -14,14 +14,16 @@ class Node {
 function solution(enroll, referral, seller, amount) {
   const MONEY = 100;
   const TEN_PERCENT = 10;
-  var answer = [];
+  var answer = new Array(enroll.length).fill(0);
 
   const sellerMap = new Map();
+  sellerMap.set("-", { earn: [], node: new Node("-", null) });
+
   for (let i = 0; i < enroll.length; i++) {
     if (referral[i] === "-") {
       sellerMap.set(enroll[i], {
         earn: [],
-        node: new Node(enroll[i], null),
+        node: new Node(enroll[i], sellerMap.get("-").node),
       });
     } else {
       sellerMap.set(enroll[i], {
@@ -37,15 +39,14 @@ function solution(enroll, referral, seller, amount) {
     // null 이거나 계산된 이익금이 1원보다 작을 때까지
     let currNode = sellerMap.get(s).node;
     while (currNode.parent !== null) {
-      const [toParent, toMe] = [
-        earn * (TEN_PERCENT / 100),
-        earn * (1 - TEN_PERCENT / 100),
-      ];
+      const toParent = Math.floor(earn * (TEN_PERCENT / 100));
+      const toMe = earn - toParent;
 
-      //   sellerMap.get(s).earn.pop();
-      sellerMap.get(s).earn.push(toMe);
+      if (toParent < 1) break;
 
-      //   sellerMap.get(currNode.parent.name).earn.pop();
+      s !== currNode.name && sellerMap.get(currNode.name).earn.pop();
+      sellerMap.get(currNode.name).earn.push(toMe);
+
       sellerMap.get(currNode.parent.name).earn.push(toParent);
 
       earn = toParent;
@@ -53,7 +54,11 @@ function solution(enroll, referral, seller, amount) {
     }
   });
 
-  console.log(sellerMap);
+  for (const [k, v] of [...sellerMap.entries()].filter((v) => v[0] !== "-")) {
+    const findIdx = enroll.findIndex((name) => name === k);
+    const totalEarn = v.earn.reduce((acc, currVal) => acc + currVal, 0);
+    answer[findIdx] = totalEarn;
+  }
 
   return answer;
 }
@@ -62,7 +67,7 @@ console.log(
   solution(
     ["john", "mary", "edward", "sam", "emily", "jaimie", "tod", "young"],
     ["-", "-", "mary", "edward", "mary", "mary", "jaimie", "edward"],
-    ["sam", "emily", "jaimie", "edward"],
-    [2, 3, 5, 4]
+    ["young", "john", "tod", "emily", "mary"],
+    [12, 4, 2, 5, 10]
   )
 );
