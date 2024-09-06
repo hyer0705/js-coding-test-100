be858d799fa2f9e2bdf9224d400563d1a2b38f9d
hyer0705 <hyer1205@gmail.com>
2024-09-06 22:16:27 +0900

    ✨feat: 미로 탈출 문제 풀이[실패]
    review
    - Lever까지 최단 경로 + End까지 최단 경로를 구하면 맞을 줄 알았으나...! 틀렸다.
    - 이 문제는 책을 보고 더 공부해야겠다!!!

--- solutions/30-미로-탈출.js
+++ solutions/30-미로-탈출.js
@@ -3,6 +3,13 @@
  * https://school.programmers.co.kr/learn/courses/30/lessons/159993
  */
 
+/**
+ * 채점 결과
+ * 정확성: 39.1
+ * 합계: 39.1 / 100.0
+ *
+ * BFS 개념은 알지만 코드로 표현하는 것을 못한다...
+ **/
 const dRow = [-1, 0, 1, 0];
 const dCol = [0, 1, 0, -1];
 
@@ -14,7 +21,7 @@ const CMD = {
   WALL: "X",
 };
 
-const isValid = (vis, row, col) => {
+const isValid = (grid, vis, row, col) => {
   const ROW = vis.length;
   const COL = vis[0].length;
 
@@ -22,23 +29,23 @@ const isValid = (vis, row, col) => {
     return false;
   }
 
-  if (vis[row][col]) {
+  if (grid[row][col] === CMD.WALL || vis[row][col][0]) {
     return false;
   }
 
   return true;
 };
 
-const BFS = (grid, vis, row, col) => {
+const BFS = (grid, vis, startCoordinates, end) => {
   const q = [];
-  let cnt = 0;
+  const [startRow, startCol] = startCoordinates;
 
-  q.push([row, col]);
-  vis[row][col] = true;
+  q.push([startRow, startCol, 0]);
+  vis[startRow][startCol] = [true, 0];
 
   while (q.length != 0) {
     const cell = q[0];
-    const [x, y] = cell;
+    const [x, y, time] = cell;
 
     // console.log(grid[x][y]);
 
@@ -48,42 +55,49 @@ const BFS = (grid, vis, row, col) => {
       const adjX = x + dRow[i];
       const adjY = y + dCol[i];
 
-      if (isValid(vis, adjX, adjY)) {
-        if (grid[adjX][adjY] === CMD.LEVER || grid[adjX][adjY] === CMD.PATH) {
-          q.push([adjX, adjY]);
-          vis[adjX][adjY] = true;
-          cnt++;
+      if (isValid(grid, vis, adjX, adjY)) {
+        if (grid[adjX][adjY] === CMD.PATH) {
+          q.push([adjX, adjY, time + 1]);
+          vis[adjX][adjY] = [true, time + 1];
+        }
+        if (grid[adjX][adjY] === end) {
+          vis[adjX][adjY] = [true, time + 1];
+
+          return vis[adjX][adjY][1];
         }
       }
     }
   }
-  return cnt + 1;
+  return -1;
 };
 
 function solution(maps) {
   const maze = maps.map((m) => m.split(""));
   const vis = Array.from({ length: maze.length }, () =>
-    new Array(maze[0].length).fill(false)
+    new Array(maze[0].length).fill([false, 0])
   );
 
   // find S
-  const findStartIndex = () => {
+  const findCoordinates = (findStr) => {
     const idx = [];
     for (let i = 0; i < maps.length; i++) {
-      if (maps[i].includes("S")) {
+      if (maps[i].includes(findStr)) {
         idx.push(i);
-        idx.push(maps[i].indexOf("S"));
+        idx.push(maps[i].indexOf(findStr));
         break;
       }
     }
     return idx;
   };
 
-  const [x, y] = findStartIndex();
-  const cnt = BFS(maze, vis, x, y);
-  if (cnt > 1) return cnt;
+  const startCoordinates = findCoordinates(CMD.START);
 
-  return -1;
+  //find L
+  const leverCoordinates = findCoordinates(CMD.LEVER);
+
+  const toLeverTime = BFS(maze, vis, startCoordinates, CMD.LEVER);
+  if (toLeverTime < 0) return -1;
+  return toLeverTime + BFS(maze, vis, leverCoordinates, CMD.EXIT);
 }
 
 console.log(solution(["SOOOL", "XXXXO", "OOOOO", "OXXXX", "OOOOE"]));
