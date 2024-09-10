eedc65511176ad894d0c576f24b75eb48d45ce16
hyer0705 <hyer1205@gmail.com>
2024-09-10 21:29:03 +0900

    ✨feat: 100. Same Tree 문제 풀이
    review
    - bfs 방식을 사용하여 풀이
    - pQueue, qQueue 변수에 각각 p와 q를 담는다.
    - while 반복문을 사용하여 p와 q를 bfs 방식으로 순회
    - 순회하면서 하나라도 null 값인 경우 false를 반환, 현재 순회하는 노드들의 값이 다를 경우 false를 반환

--- leetcode75/100-Same-Tree.js
+++ leetcode75/100-Same-Tree.js
@@ -13,47 +13,40 @@
  * }
  */
 /**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
  * @param {TreeNode} p
  * @param {TreeNode} q
  * @return {boolean}
  */
 var isSameTree = function (p, q) {
-  const queue = [p];
-  const res = [];
-
-  let curr;
-  while (queue.length) {
-    curr = queue.shift();
-    res.push(curr.val);
-
-    if (curr.left) {
-      queue.push(curr.left);
-    }
-    // else res.push(null);
-    if (curr.right) {
-      queue.push(curr.right);
-    }
-    // else res.push(null);
-  }
-
-  console.log(res);
-
+  const pQueue = [p];
   const qQueue = [q];
-  const res2 = [];
-  let currNode;
-  while (qQueue.length) {
-    currNode = qQueue.shift();
-    res2.push(currNode.val);
 
-    if (currNode.left) {
-      qQueue.push(currNode.left);
+  let pCurr;
+  let qCurr;
+
+  while (pQueue.length > 0 || qQueue.length > 0) {
+    pCurr = pQueue.shift();
+    qCurr = qQueue.shift();
+
+    if (pCurr !== null && qCurr !== null) {
+      if (pCurr.val !== qCurr.val) return false;
+
+      pQueue.push(pCurr.left);
+      pQueue.push(pCurr.right);
+
+      qQueue.push(qCurr.left);
+      qQueue.push(qCurr.right);
     }
-    // else res2.push(null);
-    if (currNode.right) {
-      qQueue.push(currNode.right);
-    }
-    // else res2.push(null);
+    if (pCurr !== null && qCurr === null) return false;
+    if (qCurr !== null && pCurr === null) return false;
   }
-
-  console.log(res2);
+  return true;
 };
