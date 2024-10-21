2d5be3cc145b825e4707de4d8d888452f479829d
조혜린 <hyer1205@gmail.com>
2024-10-21 23:23:12 +0900

    🚧 in progress: 42. 게임 맵 최단 거리 문제 풀이 중
    - bfs 로 풀이를 했는데 테스트 케이스를 모두 통과하지 못했다...내일 다시 풀어봐야겠다.

---
+++ solutions/42-게임-맵-최단거리.js
@@ -0,0 +1,123 @@
+/**
+ * 게임 맵 최단거리
+ * url: https://school.programmers.co.kr/learn/courses/30/lessons/1844?language=javascript
+ *
+ */
+
+/**
+ * 1차 채점 결과
+ *
+ * 정확성: 34.6
+ * 효율성: 22.6
+ * 합계: 57.1 / 100.0
+ */
+
+class Queue {
+  constructor() {
+    this.queue = [];
+    this.top = -1;
+  }
+
+  enqueue(data) {
+    this.queue.push(data);
+    this.top++;
+  }
+
+  dequeue() {
+    if (this.isEmpty()) {
+      return;
+    }
+
+    const peek = this.queue.shift();
+    this.top--;
+
+    return peek;
+  }
+
+  isEmpty() {
+    return this.queue.length === 0;
+  }
+
+  peek() {
+    if (this.isEmpty()) {
+      return;
+    }
+
+    return this.queue[0];
+  }
+}
+
+function solution(maps) {
+  let answer = 0;
+
+  // top, bottom, left, right
+  const dx = [0, 0, -1, 1];
+  const dy = [-1, 1, 0, 0];
+
+  const queue = new Queue();
+  const visited = new Set();
+
+  const N = maps.length;
+  const M = maps[0].length;
+
+  queue.enqueue([0, 0, 1]); // [x, y, 최단 거리?]
+  visited.add("0,0");
+
+  const isValid = (x, y) =>
+    x >= 0 &&
+    y >= 0 &&
+    x < maps.length &&
+    y < maps.length &&
+    !visited.has(`${x},${y}`) &&
+    maps[x][y] === 1;
+  while (!queue.isEmpty()) {
+    const [x, y, distance] = queue.dequeue();
+
+    if (x == N - 1 && y == M - 1) {
+      answer = distance;
+      break;
+    }
+    // if (distance > answer) answer = distance;
+
+    for (let i = 0; i < dx.length; i++) {
+      const [adjX, adjY] = [x + dx[i], y + dy[i]];
+
+      if (isValid(adjX, adjY)) {
+        queue.enqueue([adjX, adjY, distance + 1]);
+        visited.add(`${adjX},${adjY}`);
+      }
+    }
+  }
+
+  return answer === 0 ? -1 : answer;
+}
+
+console.log(
+  solution([
+    [1, 0, 1, 1, 1],
+    [1, 0, 1, 0, 1],
+    [1, 0, 1, 1, 1],
+    [1, 1, 1, 0, 1],
+    [0, 0, 0, 0, 1],
+  ])
+); // 11
+
+console.log(
+  solution([
+    [1, 0, 1, 1, 1],
+    [1, 0, 1, 0, 1],
+    [1, 0, 1, 1, 1],
+    [1, 1, 1, 0, 0],
+    [0, 0, 0, 0, 1],
+  ])
+); // -1
+
+console.log(
+  solution([
+    [1, 1, 1, 1, 0],
+    [1, 1, 1, 1, 0],
+    [1, 1, 1, 1, 0],
+    [1, 1, 1, 0, 0],
+    [1, 1, 1, 1, 1],
+  ])
+); // 9
